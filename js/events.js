5483d5e636992c95b66c5c5be40e3049232eb834
Jennifer Snyder <jlsnyder09@gmail.com>
2018-05-02 23:20:21 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,6 +10,12 @@ function frameIt() {
   });
 }
 
+function pressIt() {
+  $('input').on('keydown', function(){
+    if(key.which)
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
