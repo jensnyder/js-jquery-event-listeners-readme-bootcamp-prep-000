99ac1b5c868eab0b33976746a72a14b197a419d4
Jennifer Snyder <jlsnyder09@gmail.com>
2018-05-02 23:21:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('input').on('keydown', function(){
+  $('input').on('keydown', function(key){
     if(key.which == 71){
       alert('G was pressed');
     }
